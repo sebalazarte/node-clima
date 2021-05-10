e18a3f08a56736013e619991bba5a59cc9b6aeb0
Sebastian Lazarte <sebalazarte@outlook.com>
2021-05-10 19:40:53 -0300

    final

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const colors = require('colors');
 
 const { inquirerMenu, leerInput, pause, listarLugares } = require('./helpers/inquirer');
 const Busquedas = require('./models/busquedas');
@@ -6,6 +7,7 @@ const Busquedas = require('./models/busquedas');
 const main = async () => {
 
     const busquedas = new Busquedas();
+    
     let opt = '';
     do {
         opt = await inquirerMenu();
@@ -21,19 +23,28 @@ const main = async () => {
                 // console.log(lugares);
 
                 const id = await listarLugares(lugares);
-                const lugarSel = lugares.find(l => l.id === id);
-                console.log(lugarSel);
+                if (id === '0') {
+                    continue;
+                }
 
+                const lugarSel = lugares.find(l => l.id === id);
+                busquedas.agregarHistorial(lugarSel.nombre);
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+                
                 //mostrar resultados
                 console.log('\nInformacion de la ciudad:\n'.green);
                 console.log('Ciudad: ', lugarSel.nombre);
                 console.log('Latitud: ', lugarSel.lat);
                 console.log('Longitud: ', lugarSel.lng);
-                console.log('Temperatura: ');
-                console.log('Minima: ');
-                console.log('Maxima: ');
+                console.log('Temperatura: ', clima.temp);
+                console.log('Minima: ', clima.min);
+                console.log('Maxima: ', clima.max);
+                console.log('Descripcion: ', clima.desc);
                 break;
             case 2:
+                busquedas.historialCapitalizado.forEach((lugar, indice) => {
+                   console.log(`${colors.green(indice + 1)}. ${lugar}`);
+                });
                 break;
 
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,10 +1,22 @@
 const axios = require('axios');
+const fs = require('fs');
+
 
 class Busquedas {
-    historial = ['Salta', 'Cerrillos', 'Buenos aires'];
+
+    historial = [];
+    dbPath = './db/database.json'
 
     constructor() {
-        //leer db si existe
+        this.leerDB();
+    }
+
+    get historialCapitalizado() {
+        return this.historial.map(lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+            return palabras.join(' ');
+        });
     }
 
     get paramsMapBox() {
@@ -12,6 +24,14 @@ class Busquedas {
             'access_token': process.env.MAPBOX_KEY,
             'limit': 5,
             'language': 'es'
+        }
+    }
+
+    get paramsWeather() {
+        return {
+            appid: process.env.OPEN_WEATHER_KEY,
+            units: 'metric',
+            language: 'es'
         }
     }
 
@@ -31,8 +51,56 @@ class Busquedas {
             }));
 
         } catch (error) {
-
+            console.log(error);
         }
+    }
+
+    async climaLugar(lat, lon) {
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: { ...this.paramsWeather, lat, lon }
+            });
+
+            const resp = await instance.get();
+            const { weather, main } = resp.data;
+
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    agregarHistorial(lugar = '') {
+        if (this.historial.includes(lugar.toLocaleLowerCase)) {
+            return;
+        }
+        this.historial = this.historial.splice(0,5);
+        this.historial.unshift(lugar.toLocaleLowerCase());
+        this.guardarDB();
+    }
+
+    guardarDB() {
+        const payload = {
+            historial: this.historial
+        }
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB = () => {
+        if (!fs.existsSync(this.dbPath)) {
+            return null;
+        }
+
+        const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8' });
+        const data = JSON.parse(info);
+        this.historial = data.historial;
     }
 
 }
